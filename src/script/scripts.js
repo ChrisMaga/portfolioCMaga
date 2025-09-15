69ec2fb0c1037396d18c3846ffefd493033621e5
ChrisMaga <chris_maga@hotmail.com>
2025-09-15 17:52:35 -0300

    atualizaçao

--- src/script/scripts.js
+++ src/script/scripts.js
@@ -1,17 +1,37 @@
-const toggleTheme = document.getElementById("toggle-theme");
+const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 
 function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  if (currentTheme === "dark") {
-    rootHtml.setAttribute("data-time", "light");
-  } else {
-    rootHtml.setAttribute("data-time", "dark");
-  }
+ currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
 
   toggleTheme.classList.toggle("bi-sun");
   toggleTheme.classList.toggle("bi-moon-stars");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+// accordion
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive
+      ? accordionItem.classList.remove("active")
+      : accordionItem.classList.add("active");
+  });
+});
+
+// menu active
+const menuLinks = document.querySelectorAll(".menu-link");
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
