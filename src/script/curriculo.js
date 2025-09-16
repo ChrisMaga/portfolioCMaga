e78a3899d86bcde804d87f09749239379d29c3fc
ChrisMaga <chris_maga@hotmail.com>
2025-09-16 18:16:17 -0300

    criaçao da tela 'curriculo' e ajustes

---
+++ src/script/curriculo.js
@@ -0,0 +1,23 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+ currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+// menu active
+const menuLinks = document.querySelectorAll(".menu-link");
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
